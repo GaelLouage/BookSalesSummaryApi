683f593dfe7f575fabf085b80dd888fb3c384077
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2025-05-16 19:31:20 +0200

    book sales summary api

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Infra.Services.Classes;
+using Infra.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IBookService>(new BookService("books"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// get summary
+app.MapGet("/GetSalesSummaryAsync",async Task<IResult> (IBookService bookService) =>
+{
+    return Results.Ok(await bookService.GetSalesSummaryAsync());
+})
+.WithName("GetSalesSummaryAsync")
+.WithOpenApi();
+//get most sold book
+app.MapGet("/GetTopSellingBook", async Task<IResult> (IBookService bookService) =>
+{
+    return Results.Ok(await bookService.GetTopSellingBook());
+})
+.WithName("GetTopSellingBook")
+.WithOpenApi();
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
